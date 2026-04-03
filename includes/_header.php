bf3e3f156868b38540a8b07ba2747e2484670a6d
Matheus <matheusviana3038@gmail.com>
2026-04-03 10:52:11 -0300

    comecei a fazer a pagina de inicio

---
+++ includes/_header.php
@@ -0,0 +1,10 @@
+<header class="_header">
+    <div class="main">
+        <h1 class="brand">TASKS MANAGER</h1>
+        <div class="perfil">
+            <a href="/">
+                <img src="" alt="perfil">
+            </a>
+        </div>
+    </div>
+</header>
