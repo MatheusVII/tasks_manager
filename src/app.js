839d4a8b7a6f051d315c688c18335d0bcc287ebc
Matheus <matheusviana3038@gmail.com>
2026-04-04 17:07:49 -0300

    criei o inicio da api

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'Rota principal'
+    })
+})
+
+
+
+export default app;
